31c7a432fc7a9d196621f9a8ce3e159940758fd9
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-30 09:22:53 +0300

    Rectangle, Square, NewSquare

--- main.py
+++ main.py
@@ -1,59 +1,34 @@
-class Date:
-    def __init__(self, Year = 1970, Month = 1, Day = 1) -> None:
-        self.Year = 1970
-        self.Month = 1
-        self.Day = 1
-        self.set_date(Year, Month, Day)
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def print(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print("*", end="")
+            print("")
+
+r = Rectangle(3, 5)
+r.print()
+
+class Square:
+    def __init__(self, dimension) -> None:
+        self.dimension = dimension
     
     def print(self):
-        print("The date is {}/{}/{}".format(self.Day, self.Month, self.Year))
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                print("*", end="")
+            print("")
 
-    def set_date(self, Year, Month, Day):
-        if Month < 1 or Month > 12:
-            print("Invalid month")
-        elif Day < 1 or Day > 31:
-            print("Invalid day")
-        # additional validation rules
-        else:
-            self.Day = Day
-            self.Month = Month
-            self.Year = Year
+s = Square(4)
+s.print()
 
-class Movie:
-    def __init__(self, name, release_year, release_month, release_day) -> None:
-        self.name = name
-        # self.release_year = release_year
-        # self.release_month = release_month
-        # self.release_day = release_day
-        self.release_date = Date(release_year, release_month, release_day)
-        self.viewers = []
-    
-    def add_viewer(self, viewer):
-        self.viewers.append(viewer)
+class NewSquare(Rectangle):
+    def __init__(self, dimension):
+        self.height = dimension
+        self.width = dimension
 
-class Viewer:
-    def __init__(self, name) -> None:
-        self.name = name
-        self.movies = []
-
-    def see_movie(self, movie):
-        self.movies.append(movie)
-        movie.add_viewer(self)
-        # this would violate the data encapsulation principle
-        # movie.viewers.append(self)
-
-
-
-joker = Movie("Joker", 2024, 10, 15)
-shrek = Movie("Shrek", 2024, 11, 28)
-inside_out = Movie("Inside Out", 2024, 8, 19)
-
-caner = Viewer("caner")
-tinaz = Viewer("tinaz")
-
-caner.see_movie(joker)
-caner.see_movie(inside_out)
-tinaz.see_movie(joker)
-tinaz.see_movie(shrek)
-
-i = 4
+ns = NewSquare(7)
+ns.print()

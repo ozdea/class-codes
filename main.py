2001ba4859479d6920f11e89829289f158e77ed1
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-30 10:46:16 +0300

    polymorphism

--- main.py
+++ main.py
@@ -1,66 +1,29 @@
-import sys
+# abstract class
+class Animal:
+    def talk(self):
+        pass
 
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+class Dog(Animal):
+    def talk(self):
+        print("Hav")
 
-    def print(self):
-        for i in range(self.height):
-            for j in range(self.width):
-                print("*", end="")
-            print("")
+class Cat(Animal):
+    def talk(self):
+        print("Miyav")
 
-r = Rectangle(3, 5)
-r.print()
+class Bird(Animal):
+    def talk(self):
+        print("Cik")
 
-class Square:
-    def __init__(self, dimension) -> None:
-        self.dimension = dimension
-    
-    def print(self):
-        for i in range(self.dimension):
-            for j in range(self.dimension):
-                print("*", end="")
-            print("")
+cat = Cat()
+bird = Bird()
+bird2 = Bird()
+dog = Dog()
 
-s = Square(4)
-s.print()
+pets = [cat, bird, bird2, dog]
 
-class NewSquare(Rectangle):
-    def __init__(self, dimension, name):
-        super().__init__(dimension, dimension)
-        # violates data encapsulation
-        # self.height = dimension
-        # self.width = dimension
-        self.name = name
+def all_talk(pets):
+    for pet in pets:
+        pet.talk()
 
-    def hello(self):
-        print(f"Hello from NewSquare called: {self.name}")
-
-    # function override
-    def print(self):
-        print("Printing NewSquare called: ", self.name)
-        super().print()
-        # violates data encapsulation
-        # for i in range(self.width):
-        #     for j in range(self.height):
-        #         print("*", end="")
-        #     print("")
-
-
-ns = NewSquare(7, "ns")
-ns.print()
-ns.hello()
-
-# everything is object in python
-
-print("issubclass(NewSquare, Rectangle)", issubclass(NewSquare, Rectangle))
-print("issubclass(Square, Rectangle)", issubclass(Square, Rectangle))
-
-print("issubclass(Square, object)", issubclass(Square, object))
-print("issubclass(int, object)", issubclass(int, object))
-
-print("issubclass(int, float)", issubclass(int, float))
-
-print("issubclass(bool, int)", issubclass(bool, int))
+all_talk(pets)

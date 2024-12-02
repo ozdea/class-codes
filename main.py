473f13e4b2bf8d01ebfbb0c2feaf0a520a07fc78
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-12-02 09:58:58 +0300

    print_recursive

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ class ListNode:
         else:
             self.next = ListNode(val)
 
+    def size_recursive(self):
+        if self.next:
+            return 1 + self.next.size_recursive()
+        else:
+            return 1
+    
+    def find_recursive(self, val):
+        if self.value == val:
+            return True
+        elif self.next == None:
+            return False
+        else:
+            return self.next.find_recursive(val)
+    
+    def print_recursive(self):
+        print(self.value, end=" ")
+        if self.next:
+            self.next.print_recursive()
+
 class List:
     def __init__(self) -> None:
         self.root = None
@@ -26,21 +45,44 @@ class List:
         else:
             self.insert_front(val)
 
-    
+    # O(n)
     def size(self):
-        pass
+        if self.root:
+            return self.root.size_recursive()
+        else:
+            return 0
 
+    # O(n)
     def find(self, val):
-        pass
+        if self.root:
+            return self.root.find_recursive(val)
+        else:
+            return False
 
     def print(self):
-        pass
+        print("[", end="")
+        if self.root:
+            self.root.print_recursive()
+        print("]")
+
 
     def remove_front(self):
         if self.root != None:
             self.root = self.root.next
 
 l = List()
+sz1 = l.size()
+print(sz1)
 l.insert_front(3)
 l.insert_front(5)
 l.insert_back(15)
+sz2 = l.size()
+print(sz2)
+
+f1 = l.find(15)
+print(f1)
+
+f2 = l.find(0)
+print(f2)
+
+l.print()

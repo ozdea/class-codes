f5a25c7ee3bb2884a60eafd5ff519ad4d904cfa0
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-12-02 09:05:25 +0300

    insert_back

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ class List:
         new_node = ListNode(val)
         new_node.next = self.root
         self.root = new_node
+
+    # O(n)
+    def insert_back(self, val):
+        if self.root == None:
+            self.insert_front(val)
+        else:
+            current_node = self.root
+            while current_node.next != None:
+                current_node = current_node.next
+            current_node.next = ListNode(val)
     
     # O(n)
     def size(self):
@@ -22,7 +32,31 @@ class List:
             current_node = current_node.next
         return count
 
+    # O(n)
+    def find(self, val):
+        current_node = self.root
+        while current_node != None:
+            if current_node.value == val:
+                return True
+            current_node = current_node.next
+        return False
+
+    # O(n)
+    def print(self):
+        print("[", end="")
+        current_node = self.root
+        while current_node != None:
+            print(current_node.value, end=" ")
+            current_node = current_node.next
+        print("]")
+
+    # O(1)
+    def remove_front(self):
+        if self.root != None:
+            self.root = self.root.next
+
 l = List()
+l.print()
 sz1 = l.size()
 print(sz1)
 l.insert_front(3)
@@ -30,3 +64,11 @@ l.insert_front(5)
 l.insert_front(7)
 sz2 = l.size()
 print(sz2)
+
+f5 = l.find(5)
+print(f5)
+f15 = l.find(15)
+print(f15)
+
+l.insert_back(15)
+l.print()

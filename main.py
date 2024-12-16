1c96b4faa0816e602a350241da19cb307562de64
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-12-16 09:34:07 +0300

    binary search

--- main.py
+++ main.py
@@ -1,53 +1,21 @@
-class HashTable:
-    def __init__(self) -> None:
-        self.BUCKET_SIZE = 100
-        self.table = [None] * self.BUCKET_SIZE
-    
-    # O(1) on average
-    def insert(self, val):
-        hashed_value = hash(val)
-        index = hashed_value % self.BUCKET_SIZE
-        if self.table[index] == None:
-            self.table[index] = []
-        self.table[index].append(val)
-
-    # O(1)
-    def find(self, val):
-        hashed_value = hash(val)
-        index = hashed_value % self.BUCKET_SIZE
-        if self.table[index] == None:
-            return False
+def binary_search(l, val):
+    left = 0
+    right = len(l) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if l[mid] == val:
+            return True
+        elif l[mid] < val:
+            left = mid + 1
         else:
-            return val in self.table[index]
+            right = mid - 1
+    return False
 
-    # O(n)    
-    def size(self):
-        total = 0
-        for l in self.table:
-            if l:
-                total += len(l)
-        return total
 
-h = HashTable()
-h.insert("caner")
-h.insert("tamer")
-h.insert("tınaz")
-h.insert("aybek")
+l = [1, 3, 7, 13, 18, 50, 75, 80, 99]
 
-f1 = h.find("caner")
-f2 = h.find("refik")
+f1 = binary_search(l, 75)
+print(f1)
 
-s = h.size()
-
-i = hash("caner")
-j = hash(45353)
-
-k = hash("Caner")
-
-print(i)
-print(k)
-
-# Python set and dict are implemented as hash tables
-s = set(["caner", "aybek"])
-
-l = 3
+f2 = binary_search(l, 15)
+print(f2)
